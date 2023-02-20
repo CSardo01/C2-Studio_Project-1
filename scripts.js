59354dc4ea72f05c7f5285698b3fd0ab1e81d6c6
CSardo01 <sardc042@newschool.edu>
2023-02-20 15:24:00 -0500

    edit transition

--- scripts.js
+++ scripts.js
@@ -4,6 +4,9 @@ const observer = new IntersectionObserver(entries => {
     entries.forEach(entry => {
         entry.target.classList.toggle("show", entry.isIntersecting)
     })
+}, {
+    root: document,
+    rootMargin: '0% 0% 0% 0%',
 })
 
 fade.forEach(fade => { observer.observe(fade) })
